0dec2b07d76398ea16582aca6c52159bec8ff36a
maksim <maks.sidenko.02@gmail.com>
2023-06-18 14:19:39 +0300

    18_06_23

---
+++ lernReact/react-project/src/Message.tsx
@@ -0,0 +1,10 @@
+function Message(){
+    const name = 'Maks'
+    if( name)
+        return <h1>whatever you want, {name}</h1>;
+    return <h1>hello world</h1>
+        
+
+}
+
+export default Message;
